df68f1040ec032e630a9021392f26cfce9bbadf1
josekim <joseph.kim85@gmail.com>
2018-06-16 14:13:03 -0500

    Add eslint, Add Routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import firebase from 'firebase';
+import { HashRouter, Route } from 'react-router-dom';
+import Start from './Start.js';
 
+const config = {
+  apiKey: 'AIzaSyCrgBJMTaiUIJi8hoPvUiMbeyiqvGLFaWo',
+  authDomain: 'coding-project-wd.firebaseapp.com',
+  databaseURL: 'https://coding-project-wd.firebaseio.com',
+  projectId: 'coding-project-wd',
+  storageBucket: 'coding-project-wd.appspot.com',
+  messagingSenderId: '985640520772'
+};
+
+firebase.initializeApp(config);
 class App extends Component {
+  state = { something: 'nothing', grace: 'great' };
   render() {
+    console.log(this.state.something);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <HashRouter>
+        <div>
+          <Route exact path="/" component={Start} />
+        </div>
+      </HashRouter>
     );
   }
 }
---
+++ src/Start.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Start = () => (
+  <div>
+    <p>Whats Up</p>
+  </div>
+);
+
+export default Start;
