2d974b6190bf2213b809437272c371b8bdbb0334
josekim <joseph.kim85@gmail.com>
2018-06-16 15:34:22 -0500

    add Random letter generator

--- src/Game.js
+++ src/Game.js
@@ -12,9 +12,34 @@ firebase.initializeApp(config);
 const database = firebase.database();
 
 class Game extends Component {
+  state = {};
+
+  createRandomLetters = () => {
+    let randomLetters = '';
+    const vowels = 'AEIOU';
+    const consonants = 'BCDFGHKJKLMNPQRSTVWXYZ';
+    const alphabet = vowels + consonants;
+    randomLetters +=
+      vowels[Math.floor(Math.random() * vowels.length)] +
+      consonants[Math.floor(Math.random() * consonants.length)] +
+      vowels[Math.floor(Math.random() * vowels.length)] +
+      consonants[Math.floor(Math.random() * consonants.length)];
+
+    while (randomLetters.length < 9) {
+      randomLetters += alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    return randomLetters
+      .split('')
+      .sort(function() {
+        return 0.5 - Math.random();
+      })
+      .join('');
+  };
+
   render() {
     database.ref('game1').set({ hello: 'world' });
     console.log(database);
+    console.log(this.createRandomLetters());
     return (
       <div>
         <p>this is game</p>
